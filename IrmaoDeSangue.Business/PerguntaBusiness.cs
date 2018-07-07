8c00bab03d8954a62d1d85b97af1b75b0bfd9494
Rodolfo Bandeira <rodolfo_band@hotmail.com>
2018-07-07 18:16:53 -0300

    Add Persistencia Questionario

---
+++ IrmaoDeSangue.Business/PerguntaBusiness.cs
@@ -0,0 +1,20 @@
+﻿using IrmaoDeSangue.Data;
+using IrmaoDeSangue.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrmaoDeSangue.Business
+{
+    public class PerguntaBusiness
+    {
+        protected PerguntaData _perguntaData { get; set; }
+
+        public IList<PerguntaEntitie> RecuperaPerguntas()
+        {
+            return _perguntaData.RecuperarPerguntas();
+        }
+    }
+}
---
+++ IrmaoDeSangue.Service/QuestionarioService.svc.cs
@@ -0,0 +1,28 @@
+﻿using IrmaoDeSangue.Entities;
+using IrmaoDeSangue.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IrmaoDeSangue.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "QuestionarioService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select QuestionarioService.svc or QuestionarioService.svc.cs at the Solution Explorer and start debugging.
+    public class QuestionarioService : IQuestionarioService
+    {
+        public PerguntaBusiness _perguntaBusiness { get; set; }
+
+        public QuestionarioService()
+        {
+            _perguntaBusiness = new PerguntaBusiness();
+        }
+
+        public IList<PerguntaEntitie> RecuperarPerguntas()
+        {
+            return _perguntaBusiness.RecuperaPerguntas();
+        }
+    }
+}
