29dde5c9d49eb9b301bb9eef0552091038ed542a
Phelipealx <40916725+Phelipealx@users.noreply.github.com>
2018-07-07 13:18:20 -0300

    add Model Questionario

---
+++ IrmaoDeSangue.MVC/Controllers/QuestionarioController.cs
@@ -0,0 +1,30 @@
+﻿using IrmaoDeSangue.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IrmaoDeSangue.MVC.Controllers
+{
+    public class QuestionarioController : Controller
+    {
+        //
+        // GET: /Questionario/
+
+        public ActionResult Index()
+        {
+            var lista = new List<QuestionarioModels>();
+            lista.Add(new QuestionarioModels { Codigo = 1, Descricao = "Teste", Tipo = 1});
+            lista.Add(new QuestionarioModels { Codigo = 2, Descricao = "Teste2", Tipo = 1});
+            lista.Add(new QuestionarioModels { Codigo = 3, Descricao = "Teste3", Tipo = 1});
+            return View(lista);
+        }
+
+        public ActionResult Visualizar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ IrmaoDeSangue.MVC/Models/QuestionarioModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrmaoDeSangue.MVC.Models
+{
+    public class QuestionarioModels
+    {
+        public int Codigo { get; set; }
+        public String Descricao { get; set; }
+        public int Tipo { get; set; }
+    }
+}
